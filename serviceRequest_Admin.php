9ae58cea715645ff81d3f1abd34acd1eeb5506c3
Van Halen Viola <vanviola08@gmail.com>
2023-06-14 18:25:37 +0800

    fixed style of service request table

--- serviceRequest_Admin.php
+++ serviceRequest_Admin.php
@@ -114,67 +114,65 @@ require_once('config/db.php');
       <div class="head-title">
         <div class="left">
           <h1>Service Request</h1>
-          <table align="center" border="1px" style="width:600px; line-height:40px;"> 
-	<tr> 
-		<th colspan="4"><h2>Service Request</h2></th> 
-		</tr> 
-			  <th>ID</th> 
-			  <th>REQUESTOR</th> 
-			  <th>DATE OF REQUEST</th> 
-			  <th>BUSINESS UNIT</th> 
-        <th>CUST./PROJECT NAME</th> 
-        <th>ASSET CODE</th> 
-        <th>MODEL</th> 
-        <th>SERIAL NO.</th> 
-        <th>EQUIP DESC</th> 
-        <th>BRAND</th> 
-        <th>SERVICE MEATER READING </th> 
-        <th>TYPE OF REQUEST</th> 
-        <th>ADDITONAL OPTION</th> 
-        <th>OTHER SERVICE REQUEST</th> 
-        <th>CHARGING</th> 
-        <th>UNIT PROBLEM</th> 
-        <th>OTHERS</th>   
-        <th>UNIT OPERATIONAL</th> 
-        <th>SPECIFIC REQUIREMENT</th> 
-        <th>ONSITE CONTACT PERSON</th> 
-        <th>FAX NO.</th> 
-			  
-		</tr> 
-		
-		<?php 
-    $query = "select * from service_requests";
-    $result = $conn->query($query);
-		if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-		?> 
-		<tr> <td><?php echo $row['ID']; ?></td> 
-		<td><?php echo $row['requestor']; ?></td> 
-    <td><?php echo $row['dateOfRequest']; ?></td> 
-    <td><?php echo $row['businessUnit']; ?></td> 
-		<td><?php echo $row['custProjectName']; ?></td> 
-		<td><?php echo $row['assetCode']; ?></td> 
-    <td><?php echo $row['model']; ?></td> 
-    <td><?php echo $row['serialNo']; ?></td> 
-    <td><?php echo $row['equipDesc']; ?></td> 
-    <td><?php echo $row['brand']; ?></td> 
-    <td><?php echo $row['serviceMeterReading']; ?></td> 
-    <td><?php echo $row['typeOfRequest']; ?></td> 
-    <td><?php echo $row['additionalOption']; ?></td> 
-    <td><?php echo $row['otherServiceRequest']; ?></td> 
-    <td><?php echo $row['charging']; ?></td> 
-    <td><?php echo $row['unitProblem']; ?></td> 
-    <td><?php echo $row['others']; ?></td> 
-    <td><?php echo $row['unitOperational']; ?></td> 
-    <td><?php echo $row['specificRequirement']; ?></td> 
-    <td><?php echo $row['onsitecontactperson']; ?></td> 
-    <td><?php echo $row['mobileOrPhoneNo']; ?></td> 
-    <td><?php echo $row['faxNo']; ?></td> 
-		</tr> 
-	<?php 
-    }
-               } 
-          ?>  
+          <table class="table-container">
+            <th>ID</th>
+            <th>REQUESTOR</th>
+            <th>DATE OF REQUEST</th>
+            <th>BUSINESS UNIT</th>
+            <th>CUST./PROJECT NAME</th>
+            <th>ASSET CODE</th>
+            <th>MODEL</th>
+            <th>SERIAL NO.</th>
+            <th>EQUIP DESC</th>
+            <th>BRAND</th>
+            <th>SERVICE MEATER READING </th>
+            <th>TYPE OF REQUEST</th>
+            <th>ADDITONAL OPTION</th>
+            <th>OTHER SERVICE REQUEST</th>
+            <th>CHARGING</th>
+            <th>UNIT PROBLEM</th>
+            <th>OTHERS</th>
+            <th>UNIT OPERATIONAL</th>
+            <th>SPECIFIC REQUIREMENT</th>
+            <th>ONSITE CONTACT PERSON</th>
+            <th>FAX NO.</th>
+
+            </tr>
+
+            <?php
+            $query = "select * from service_requests";
+            $result = $conn->query($query);
+            if ($result->num_rows > 0) {
+              while ($row = $result->fetch_assoc()) {
+            ?>
+                <tr>
+                  <td><?php echo $row['ID']; ?></td>
+                  <td><?php echo $row['requestor']; ?></td>
+                  <td><?php echo $row['dateOfRequest']; ?></td>
+                  <td><?php echo $row['businessUnit']; ?></td>
+                  <td><?php echo $row['custProjectName']; ?></td>
+                  <td><?php echo $row['assetCode']; ?></td>
+                  <td><?php echo $row['model']; ?></td>
+                  <td><?php echo $row['serialNo']; ?></td>
+                  <td><?php echo $row['equipDesc']; ?></td>
+                  <td><?php echo $row['brand']; ?></td>
+                  <td><?php echo $row['serviceMeterReading']; ?></td>
+                  <td><?php echo $row['typeOfRequest']; ?></td>
+                  <td><?php echo $row['additionalOption']; ?></td>
+                  <td><?php echo $row['otherServiceRequest']; ?></td>
+                  <td><?php echo $row['charging']; ?></td>
+                  <td><?php echo $row['unitProblem']; ?></td>
+                  <td><?php echo $row['others']; ?></td>
+                  <td><?php echo $row['unitOperational']; ?></td>
+                  <td><?php echo $row['specificRequirement']; ?></td>
+                  <td><?php echo $row['onsitecontactperson']; ?></td>
+                  <td><?php echo $row['mobileOrPhoneNo']; ?></td>
+                  <td><?php echo $row['faxNo']; ?></td>
+                </tr>
+            <?php
+              }
+            }
+            ?>
 
         </div>
       </div>
