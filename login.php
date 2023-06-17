561537851af1c1d1db89dbb747066c048b32939d
Van Halen Viola <vanviola08@gmail.com>
2023-06-17 09:48:31 +0800

    ayos na

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 if (isset($_SESSION['email'])) {
-  header("location:index.html");
+  header("location:service-request.html");
 }
 
 $errors = array();
@@ -22,24 +22,22 @@ if (isset($_POST['login'])) {
   $sql = "SELECT * FROM registration WHERE email='$email' AND pass_word='$pass_word'";
   echo $sql;
   $result = $conn->query($sql);
-                    $row = $result->fetch_assoc();
-                    if ($result->num_rows == 1) {
-                        if (session_status() === PHP_SESSION_ACTIVE) {
-                            $_SESSION['email'] = $row['email'];
-                            $_SESSION['password'] = $row['pass_word'];
-                            header("Location: login-success.html");
-                            header('Location: http://' . $_SERVER['HTTP_HOST'] . '/test/service-request.html');
-                            
-                            // $_SESSION['password'] = $row['Password'];
-                        } else {
-                            echo "not started";
-                        }
-                    }else{
-                      $message = "Incorrect username/password";
-                      echo "<script>alert('$message');</script>";
-                        header('Location: http://' . $_SERVER['HTTP_HOST'] . '/test/login.html');
-                        echo "Incorrect username/password";
-                    }
-}
+  $row = $result->fetch_assoc();
+  if ($result->num_rows == 1) {
+    if (session_status() === PHP_SESSION_ACTIVE) {
+      $_SESSION['email'] = $row['email'];
+      $_SESSION['password'] = $row['pass_word'];
+      header("Location: login-success.html");
+      header('Location: http://' . $_SERVER['HTTP_HOST'] . '/test/service-request.html');
 
-?>
+      // $_SESSION['password'] = $row['Password'];
+    } else {
+      echo "not started";
+    }
+  } else {
+    $message = "Incorrect username/password";
+    echo "<script>alert('$message');</script>";
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/test/login.html');
+    echo "Incorrect username/password";
+  }
+}
