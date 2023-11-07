51484d94f00e930a06a1d5e1130c742cf2535cff
Van Halen Viola <vanviola08@gmail.com>
2023-11-07 22:47:02 +0800

    put custome loader

--- admin/workOrder_Admin.php
+++ admin/workOrder_Admin.php
@@ -27,6 +27,10 @@ if (!isset($_SESSION['username']))
 
 <body>
 
+  <div class="loader">
+    <div class="custom-loader"></div>
+  </div>
+
   <?php include 'includes/sidebar.php'; ?>
 
   <section id="content">
