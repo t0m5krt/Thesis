20eebaf03b39cf8f0ecc4808d106d48d944a785e
t0m5krt <91048191+t0m5krt@users.noreply.github.com>
2023-11-22 11:21:47 +0800

    href typo

--- employee/profile.php
+++ employee/profile.php
@@ -27,7 +27,7 @@ if ($result && mysqli_num_rows($result) > 0) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="Styles/style.css" />
+    <link rel="stylesheet" href="styles/style.css" />
     <!-- Boxicons -->
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 
--- employee/service_report_employee.php
+++ employee/service_report_employee.php
@@ -14,7 +14,7 @@ include 'includes/header.php';
     <title><?php echo TITLE ?></title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="Styles/style.css">
+    <link rel="stylesheet" href="styles/style.css">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 </head>
 
